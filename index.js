34c870b343338a42637acef78dc99f64a2ccbeb7
arnob <farhanasfar21@gmail.com>
2024-10-03 02:05:57 +0600

    Fetch user's all repos name

--- index.js
+++ index.js
@@ -6,25 +6,39 @@ async function fetchUserGithubActivity(userName){
 
     try{
         const response = await fetch(url);
-        console.log(response)
+        //console.log(response)
         if(!response.ok){
             throw new Error(`Status: ${response.status}`);
         }
         
         const userInfo = await response.json();
 
-        displayUserActivity(userInfo);
+        //displayUserActivity(userInfo);
     }catch(error){
         console.error(error.message);
     }
 }
 
-function displayUserActivity(userInfo){
-    console.log("success")
-    // console.log('User Activity:');
-    // userInfo.slice(0, 5).forEach((event, index) => {
-    //     console.log(`${index + 1}. Type: ${event.type}, Repo: ${event.repo.name}, Created at: ${event.created_at}`);
-    // });
+async function fetchUserRepos(userName) {
+    const url = `https://api.github.com/users/${userName}/repos`;
+
+    try{
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`Status: ${response.status}`);
+        }
+        const userRepos = await response.json();
+        displayUserActivity(userRepos);
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+function displayUserActivity(events){
+    //console.log(userRepos)
+    events.forEach(event=>{
+        console.log(event.full_name);
+    });
     
 }
 
@@ -33,6 +47,7 @@ const userName = process.argv[2];
 if(!userName){
     console.log("Please enter a github user name");
 }else{
-    fetchUserGithubActivity(userName);
+    //fetchUserGithubActivity(userName);
+    fetchUserRepos(userName);
 }
 
