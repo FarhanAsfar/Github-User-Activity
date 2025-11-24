c3291dd0fb31f57d08e0d24123e7b7083fbbabc8
arnob <farhanasfar21@gmail.com>
2025-11-24 16:16:31 +0600

    fix

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 //allows the script to be run directly from command line
 
-async function fetchUserGithubActivity(userName){
+async function fetchUserGithubActivity(userName) {
     const url = `https://api.github.com/users/${userName}/events`;
 
-    try{
+    try {
         const response = await fetch(url);
         //console.log(response)
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error(`Status: ${response.status}`);
-        }  
+        }
         const userInfo = await response.json();
 
         displayUserActivity(userInfo);
-    }catch(error){
+    } catch (error) {
         console.error(error.message);
     }
 }
@@ -21,26 +21,26 @@ async function fetchUserGithubActivity(userName){
 async function fetchUserRepos(userName) {
     const url = `https://api.github.com/users/${userName}/repos`;
 
-    try{
+    try {
         const response = await fetch(url);
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error(`Status: ${response.status}`);
         }
         const userRepos = await response.json();
         displayUserRepos(userRepos);
-    }catch(error){
+    } catch (error) {
         console.error(error.message);
     }
 }
-function displayUserActivity(userInfo){
+function displayUserActivity(userInfo) {
     //console.log(userInfo);
     console.log("----------Recent Activites------------\n--------------------------------------");
 
-    if(userInfo.length == 0){
+    if (userInfo.length == 0) {
         console.log("No recent activity!");
     }
-    else{
-        userInfo.forEach(events =>{
+    else {
+        userInfo.forEach(events => {
             console.log(`Type: ${events.type},      Repo: ${events.repo.name.split('/')[1]},        Created: ${events.created_at}`);
         });
     }
@@ -48,19 +48,19 @@ function displayUserActivity(userInfo){
 }
 
 
-function displayUserRepos(userRepos){
+function displayUserRepos(userRepos) {
     console.log("------User Repositories---------\n--------------------------------")
-    userRepos.forEach(event=>{
+    userRepos.forEach(event => {
         console.log(event.full_name.split('/')[1]);
     });
-    
+
 }
 
 
 const userName = process.argv[2];
-if(!userName){
+if (!userName) {
     console.log("Please enter a github user name");
-}else{
+} else {
     fetchUserGithubActivity(userName);
     fetchUserRepos(userName);
 }
